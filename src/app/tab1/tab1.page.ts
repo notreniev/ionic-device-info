e8a7dc72fdcbc48710e5a4fd1400243ecd216ea8
everton-canez <everton-canez@procergs.rs.gov.br>
2019-08-20 16:41:20 -0300

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,19 @@
+import { Platform } from '@ionic/angular';
 import { Component } from '@angular/core';
+
+declare let device: any
+
+interface deviceInterface {
+  uuid?: string,
+  model?: string,
+  cordova?: string,
+  platform?: string,
+  version?: string,
+  manufacturer?: string,
+  serial?: string,
+  isVirtual?: boolean,
+
+};
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +22,24 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public deviceInfo: deviceInterface = {};
 
+  constructor(private platform: Platform) {
+    this.platform.ready()
+      .then(
+        res => {
+          this.deviceInfo.uuid = device.uuid
+          this.deviceInfo.model = device.model
+          this.deviceInfo.cordova = device.cordova
+          this.deviceInfo.platform = device.platform
+          this.deviceInfo.version = device.version
+          this.deviceInfo.manufacturer = device.manufacturer
+          this.deviceInfo.serial = device.serial
+          this.deviceInfo.isVirtual = device.isVirtual
+        }
+      ).catch(err => {
+        console.clear()
+        console.error('Isso só funciona em um dispositivo android!')
+      })
+  }
 }
